4793ab7ea76c868131910b6802aa505ac8e4b49b
MohammadHossein Labafbashi <92016155+Labafbashi@users.noreply.github.com>
2022-12-13 09:54:45 +0100

    405 error

--- script/BishBosh.js
+++ script/BishBosh.js
@@ -1,21 +1,46 @@
 // This code executed on the console.
-for (let i = 0; i < 100; i++) {
-  answer = i;
-  if (i % 3 == 0) {
-    answer = "Bish";
-  }
-  if (i % 4 == 0) {
-    answer = "Bosh";
-  }
-  if (i % 3 == 0 && i % 4 == 0) {
-    answer = "Bish-Bosh";
-  }
-  console.log(answer);
-}
+// for (let i = 0; i < 100; i++) {
+//   answer = i;
+//   if (i % 3 == 0) {
+//     answer = "Bish";
+//   }
+//   if (i % 4 == 0) {
+//     answer = "Bosh";
+//   }
+//   if (i % 3 == 0 && i % 4 == 0) {
+//     answer = "Bish-Bosh";
+//   }
+//   console.log(answer);
+// }
 
-function chkBishValue(e) {
-  return (
-    (e.charCode != 8 && e.charCode == 0) ||
-    (e.charCode >= 48 && e.charCode <= 57)
+
+
+
+function calculateBishBosh(bish, bosh, count) {
+  form1 = document.getElementById("bishBoshAnswer").value;
+  
+  //   counter = document.getElementById(countValue);
+  //   bish = document.getElementById(bishValue);
+  //   bosh = document.getElementById(boshValue);
+  var finalAnswer = "";
+
+  for (let i = 0; i <= count; i++) {
+    answer = i;
+    if (i % bish == 0) {
+      answer = "Bish";
+    }
+    if (i % bosh == 0) {
+      answer = "Bosh";
+    }
+    if (i % bish == 0 && i % bosh == 0) {
+      answer = "Bish-Bosh";
+    }
+    console.log(answer);
+    finalAnswer = finalAnswer + ", " + answer;
+  }
+  console.log(
+    "Just for test!!! " + bish + " " + bosh + " " + count + " " + form1
   );
+  form1 = finalAnswer;
+  return true;
 }
